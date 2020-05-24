2abcc283ddb7338deb3b5eb6f8186fff99468205
Work <anisbhati101@gmail.com>
2020-05-24 12:45:55 +0530

    first commit

---
+++ src/pattern/Main.java
@@ -0,0 +1,32 @@
+package pattern;
+
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+	
+	 
+    public static void main(String[] args) {
+    
+    	for(int i=0;i<3;i++) {
+    		for(int j=0;j<2-i;j++) {
+    			System.out.print(j);
+    		}
+    		for(int k=0;k<=i;k++) {
+    			System.out.print("*");
+    		//   01*
+    		//   0***
+    		//   *****
+    		}
+    		
+    		for(int l=1;l<=i;l++) {
+    			System.out.print("*");
+    		}
+    		
+    		System.out.println();
+    	}
+
+	}
+}
